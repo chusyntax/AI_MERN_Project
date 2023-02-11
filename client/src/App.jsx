7093da839cabf628b4a8e071be339bb27c6b2ba4
Chu <ttheko101@gmail.com>
2023-02-11 20:00:59 +0200

    Added Front-end setup for project

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () =>{
+    return (
+
+        <h1 className="text-3xl font-bold underline">
+        Hello world! Its me
+      </h1>
+
+    )
+}
+
+export default App
