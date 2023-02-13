7c2650e3c22b91d3d3d680b934b32c7d7b566f13
Chu <ttheko101@gmail.com>
2023-02-13 21:46:00 +0200

    Continued woth the project

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -20,7 +20,13 @@ bg-[#6469ff] text-white px-4 py-2 rounded-full'>Create
 </Link>
 
 </header>
+<main className="sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]">
+<Routes>
+<Route path="/" element={<Home />}/>
+<Route path="/create-post" element={<CreatePost />}/>
 
+</Routes>
+</main>
 </BrowserRouter>
 
     )
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import {Loader, Card, FormField} from '../components'
 
 const Home = () => {
+    //const [loading, setLoading] = useSate(false)
+    //const [allPosts, setAllPosts] = useState(null)
   return (
-    <div>Home</div>
+    <section className="max-w-7xl mx-auto">
+
+     <div>
+        <h1 className='font-extrabold text-[#222328] text-[32px]'>Posts from The Community</h1>
+        <p className='mt-2 text-[#666e75] text-[16px] max-w-[500px]'>Browse through all the images within the  collection. Every image has been generated using the DALL-E AI</p>
+     </div>
+
+     <div className='mt-16'>
+        <FormField/>
+     </div>
+
+    </section>
   )
 }
 
