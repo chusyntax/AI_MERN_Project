cb6b501f7e706b013bac8c7d83f8432f5f3ec1e8
Chu <ttheko101@gmail.com>
2023-02-16 22:28:45 +0200

    COmpleted skeleton of FormField page

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,8 +1,64 @@
 import React from 'react'
+import {useNavigate} from 'react-router-dom';
+import {useState} from 'react'
+
+import {preview} from '../assets';
+import {getRandomPrompt} from '../utils';
+import {FormField, Loader} from '../components'
+
 
 const CreatePost = () => {
+   const navigate = useNavigate();
+  const [form, setForm] = useState({
+    name:'',
+    prompt:'',
+    photo:'',
+  });
+  const [generatingImg, setGeneratingImg] = useState(false);
+  const [loading, setLoading] = useState(false); 
+  const handleSubmit =()=>{
+
+  }
+  const handleChange =(e)=>{
+
+  }
+  const handleSurpriseMe =(e)=>{
+
+  }
   return (
-    <div>CreatePost</div>
+    <section className="max-w-7xl mx-auto">
+
+<div>
+        <h1 className='font-extrabold text-[#222328] text-[32px]'>Create</h1>
+        <p className='mt-2 text-[#666e75] text-[16px] max-w-[500px]'>Create your very own images using Open-AI and DALL-E, share them with the community and download them as well.</p>
+     </div>
+
+     <form className="mt-16 max-w-3xl" onSubmit={handleSubmit}>
+
+    <div className="flex flex-col gap-5">
+      <FormField
+      labelName="Your name"
+      type="text"
+      name="name"
+      placeholder="John Doe"
+      value={form.name}
+      handleChange={handleChange}
+      />
+      <FormField
+      labelName="Prompt"
+      type="text"
+      name="Prompt"
+      placeholder="A Samurai riding a Horse on Mars, lomography."
+      value={form.prompt}
+      handleChange={handleChange}
+      isSurpriseMe
+      handleSurpriseMe={handleSurpriseMe}
+      />
+    </div>
+
+     </form>
+
+    </section>
   )
 }
 
